5556d50a147951a6a6838323bfe0771263a712f8
singhsahab1981 <singh.sahab1981@yahoo.com>
2022-04-25 00:20:41 +0530

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title:'New React Project',
-        description:'This is my firts website',
-        keywords: 'React, React New Project'
+        
     },
     plugins: [
         `gatsby-plugin-react-helmet`
